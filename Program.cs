176b4c91db73098b370429c3cc436792fa8ab1ba
hugntran <133189997+hugntran@users.noreply.github.com>
2024-10-08 13:19:57 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,54 @@
-using DMAWS_T2305M_TranHung.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using DMAWS_T2305M_TranHung.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add CORS policy access
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        //policy.WithOrigins("http://localhost:3000");
+        policy.AllowAnyOrigin();
+        //policy.WithMethods("POST");
+        policy.AllowAnyMethod();
+        policy.AllowAnyHeader();
+    });
+});
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// add AUTH JWT Bearer
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(
+        options =>
+        {
+            string key = builder.Configuration["JWT:Key"];
+            string issuerX = builder.Configuration["JWT:Issuer"];
+            string audienceX = builder.Configuration["JWT:Audience"];
+            int lifeTime = Convert.ToInt32(builder.Configuration["JWT:Lifetime"]);
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                ValidIssuer = issuerX,
+                ValidAudience = audienceX,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+            };
+        }
+    );
+
+// connect db
+T2305mApiContext.ConnectionString = builder.Configuration.GetConnectionString("T2305M_API");
+builder.Services.AddDbContext<T2305mApiContext>(
+    options => options.UseSqlServer(T2305mApiContext.ConnectionString)
+    );
+// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,10 +60,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -29,3 +72,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
