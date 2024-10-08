04ab6c2b88d37581f2bc5e0fdf0d53a6afecfe12
hugntran <133189997+hugntran@users.noreply.github.com>
2024-10-08 13:16:22 +0700

    Update and rename EmployeeDetailsDto.cs to EmployeeDTO.cs

---
+++ Models/Dto/EmployeeDTO.cs
@@ -0,0 +1,26 @@
+namespace DMAWS_T2305M_TranHung.Models.Dto
+{
+	public class GetBasicEmployeeDTO
+	{
+		public int EmployeeId { get; set; }
+		public string EmployeeName { get; set; }
+		public DateTime EmployeeDOB { get; set; }
+		public string EmployeeDepartment { get; set; }
+	}
+
+	public class GetDetailEmployeeDTO
+	{
+		public int EmployeeId { get; set; }
+		public string EmployeeName { get; set; }
+		public DateTime EmployeeDOB { get; set; }
+		public string EmployeeDepartment { get; set; }
+		public List<GetBasicProjectDTO> BasicProjectDTOs { get; set; }
+	}
+
+	public class EmployeeQueryParameters
+	{
+		public string? EmployeeName { get; set; }
+		public DateTime? EmployeeDOBFromDate { get; set; }
+		public DateTime? EmployeeDOBToDate { get; set; }
+	}
+}
