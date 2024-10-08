9cac0c226b2b46cb2bc4d796b57c47a49dbf344a
hugntran <133189997+hugntran@users.noreply.github.com>
2024-10-08 13:16:54 +0700

    Update and rename ProjectDto.cs to ProjectDTO.cs

---
+++ Models/Dto/ProjectDTO.cs
@@ -0,0 +1,25 @@
+namespace DMAWS_T2305M_TranHung.Models.Dto
+{
+	public class GetBasicProjectDTO
+	{
+		public int ProjectId { get; set; }
+		public string ProjectName { get; set; }
+		public DateTime ProjectStartDate { get; set; }
+		public DateTime? ProjectEndDate { get; set; }
+	}
+	public class GetDetailProjectDTO
+	{
+		public int ProjectId { get; set; }
+		public string ProjectName { get; set; }
+		public DateTime ProjectStartDate { get; set; }
+		public DateTime? ProjectEndDate { get; set; }
+		public List<GetBasicEmployeeDTO> Employees { get; set; }
+	}
+
+	public class ProjectQueryParameters
+	{
+		public bool? IsProjectInProgress { get; set; }
+		public bool? IsProjectInFinished { get; set; }
+		public string? ProjectName { get; set; }
+	}
+}
