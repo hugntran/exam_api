7b4b18e0ef0ad5d192932e70004b2d0198acf7e3
hugntran <133189997+hugntran@users.noreply.github.com>
2024-10-08 13:18:18 +0700

    Update ProjectEmployee.cs

--- Models/ProjectEmployee.cs
+++ Models/ProjectEmployee.cs
@@ -1,12 +1,19 @@
-﻿namespace DMAWS_T2305M_TranHung.Models { 
+using System.ComponentModel.DataAnnotations;
 
-public class ProjectEmployee
+namespace DMAWS_T2305M_TranHung.Models
 {
-	public int EmployeeId { get; set; } // Id nhân viên
-	public int ProjectId { get; set; } // Id dự án
-	public string Tasks { get; set; } // Nhiệm vụ trong dự án
+	public class ProjectEmployee
+	{
+		[Required]
+		public int EmployeeId { get; set; }
 
-	public virtual Employee Employees { get; set; } // Nhân viên
-	public virtual Project Projects { get; set; } // Dự án
-}
+		[Required]
+		public int ProjectId { get; set; }
+
+		[Required(ErrorMessage = "Tasks are required.")]
+		public string Tasks { get; set; }
+
+		public virtual Employee? Employees { get; set; }
+		public virtual Project? Projects { get; set; }
 	}
+}
